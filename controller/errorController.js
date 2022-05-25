8219b4ad6ac6e771c54496e4551ad1811256c0e7
lamgnaoh <luonglamhoang2000@gmail.com>
2022-05-25 17:39:37 +0700

    error handling duplicate field DB

--- controller/errorController.js
+++ controller/errorController.js
@@ -4,7 +4,11 @@ const handleCastErrorDB = (err) => {
   const message = `Invalid ${err.path}: ${err.value}`;
   return new AppError(message, 400);
 };
-
+const handleDuplicateErrorDB = (err) => {
+  const field = err.errmsg.match(/"(.*?)"/)[0];
+  const message = `Duplicate field value ${field}. Please use another value`;
+  return new AppError(message, 404);
+};
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
@@ -19,7 +23,9 @@ module.exports = (err, req, res, next) => {
   } else if (process.env.NODE_ENV === "production") {
     let error = { ...err };
     error.name = err.name;
+    error.errmsg = err.errmsg;
     if (error.name === "CastError") error = handleCastErrorDB(error);
+    if (error.code === 11000) error = handleDuplicateErrorDB(error);
     // operational error
     if (error.isOperational) {
       res.status(error.statusCode).json({
