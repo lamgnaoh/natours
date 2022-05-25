f890589011a1f924724e155c05a5f75004427cdc
lamgnaoh <luonglamhoang2000@gmail.com>
2022-05-25 17:15:43 +0700

    error handling invalid DB id

--- controller/errorController.js
+++ controller/errorController.js
@@ -1,3 +1,10 @@
+const AppError = require("../utils/appError");
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppError(message, 400);
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
@@ -10,16 +17,19 @@ module.exports = (err, req, res, next) => {
       stack: err.stack,
     });
   } else if (process.env.NODE_ENV === "production") {
+    let error = { ...err };
+    error.name = err.name;
+    if (error.name === "CastError") error = handleCastErrorDB(error);
     // operational error
-    if (err.isOperational) {
-      res.status(err.statusCode).json({
-        status: err.status,
-        message: err.message,
+    if (error.isOperational) {
+      res.status(error.statusCode).json({
+        status: error.status,
+        message: error.message,
       });
       // programming error
     } else {
       // 1. Log error
-      console.error(err);
+      console.error(error);
       // 2. Send generic message
       res.status(500).json({
         status: "error",
--- server.js
+++ server.js
@@ -26,6 +26,7 @@ mongoose
 
 //4, start server
 const port = process.env.PORT || 3000;
+
 app.listen(port, () => {
   console.log(`Listening on port ${port} ... `);
 });
